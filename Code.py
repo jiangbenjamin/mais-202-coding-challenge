e4b5562927679a7e5d6336707f43b047ac3037fc
jiangbenjamin <46290125+jiangbenjamin@users.noreply.github.com>
2018-12-31 21:58:56 -0500

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,12 @@
+mport pandas as pd
+import seaborn as sns
+
+data = pd.read_csv(r'https://raw.githubusercontent.com/McGillAISociety/mais-202-coding-challenge/master/data.csv')
+
+average = data.groupby(data.purpose).int_rate.mean()
+average = average.reset_index()
+
+figure = sns.barplot(x = 'purpose', y = 'int_rate', data = average)
+figure.set_xticklabels(figure.get_xticklabels(), rotation = 270)
+
+print(average.to_string())
